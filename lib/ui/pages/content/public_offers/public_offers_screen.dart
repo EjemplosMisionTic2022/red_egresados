72014a81c62ca7522dc3f4f71de576f8cbb57ad9
kristellu <kristellu@uninorte.edu.co>
2021-11-05 13:49:04 -0500

    Added todo stateful widgets

--- lib/ui/pages/content/public_offers/public_offers_screen.dart
+++ lib/ui/pages/content/public_offers/public_offers_screen.dart
@@ -5,29 +5,19 @@ class PublicOffersScreen extends StatefulWidget {
   // PublicOffersScreen empty constructor
   const PublicOffersScreen({Key? key}) : super(key: key);
 
-  @override
-  _State createState() => _State();
+  // Completa el código con las lineamientos del instructor
+  // ---------------------------------------
+  // 1. Crea los estados del widget usando createState()
+  // ---------------------------------------
 }
 
+// ---------------------------------------
+// 2. Define el estado del Widget 
+// ---------------------------------------
 class _State extends State<PublicOffersScreen> {
-  final items = List<String>.generate(20, (i) => "Item $i");
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return OfferCard(
-          title: 'Desarrollador backend de NodeJs',
-          content:
-              'Estamos buscando un desarrollador backend independiente de NodeJS para trabajar en el backend de una aplicación nativa de Android. Alguien que pueda ayudarnos en la creación de una aplicación móvil de redes sociales basada en video desde cero. Este sería un puesto remunerado con la opción de trabajar desde casa. Una oportunidad de aprender y desarrollar algo desde cero.',
-          arch: 'Android',
-          level: 'Experto',
-          payment: 3000000,
-          onCopy: () => {},
-          onApply: () => {},
-        );
-      },
-    );
-  }
+  // ---------------------------------------
+  // 3. Genera la lista de datos que se mostrarán en cada card/tarjeta
+  // ---------------------------------------
+  // 4. Define la estructura del widget utilizando el metodo Build
+  // ---------------------------------------
 }
