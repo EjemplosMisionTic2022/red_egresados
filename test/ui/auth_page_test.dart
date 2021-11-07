0cc4b23dccee02c5facbfabe056d2f9698442f89
Franklin E. Zabaleta <franklinz@uninorte.edu.co>
2021-11-07 18:34:05 -0500

    more widget test tweaks

--- test/ui/auth_page_test.dart
+++ test/ui/auth_page_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:red_egresados/domain/use_cases/controllers/authentication.dart';
+import 'package:red_egresados/domain/use_cases/controllers/connectivity.dart';
 import 'package:red_egresados/ui/pages/authentication/auth_page.dart';
 
 void main() {
@@ -8,9 +10,14 @@ void main() {
       TestWidgetsFlutterBinding.ensureInitialized()
           as TestWidgetsFlutterBinding;
 
-  testWidgets("login-screen", (WidgetTester tester) async {
+  setUp(() {
     binding.window.physicalSizeTestValue = const Size(1080, 1920);
     binding.window.devicePixelRatioTestValue = 1.0;
+    Get.put(AuthController());
+    Get.put(ConnectivityController());
+  });
+
+  testWidgets("login-screen", (WidgetTester tester) async {
     // Widgets Testing requires that the widgets we need to test have a unique key
     final emailField = find.byKey(const ValueKey("signInEmail"));
     final passwordField = find.byKey(const ValueKey("signInPassword"));
